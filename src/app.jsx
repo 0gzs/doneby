234037a6f247d59d62122156fa6b5f5d4a31d6c6
0gzs <r.jesus9107@gmail.com>
2025-05-20 13:32:18 -0700

    Add TimeCalculator to app.js and pass formData

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react"
 import RoomInputForm from "./components/RoomInputForm"
+import TimeCalculator from "./components/TimeCalculator"
 
 function App() {
   const [formData, setFormData] = useState({
     startTime: '8:00 AM',
     checkouts: 0,
     fullService: 0,
-    stayovers: 0
+    stayoverService: 0
   })
+
+  const [showResult, setShowResult] = useState(false)
+
+  const handleCalculate = () => setShowResult(true)
+
   return (
     <div className="app">
+      <h1>DoneBy</h1>
       <RoomInputForm onFormChange={setFormData} />
-      <button className="calculate-btn">Calculate</button>
+      <button className="calculate-btn" onClick={handleCalculate}>Calculate</button>
+
+      {showResult && (
+        <TimeCalculator formData={formData} />
+      )}
     </div>
   )
 }
